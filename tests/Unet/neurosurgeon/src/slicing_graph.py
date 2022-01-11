0f92b3c568e78f1721c2e9a7f741eeb4cf232748
Joonho Wohn <givenarc@gmail.com>
2022-01-11 14:31:39 +0900

    [neurosurgeon] add graph config

--- tests/Unet/neurosurgeon/src/slicing_graph.py
+++ tests/Unet/neurosurgeon/src/slicing_graph.py
@@ -10,6 +10,7 @@ import pygraphviz as pgv
 
 np.random.seed(0)
 img_size = 128
+img_size = 512
 input_data = np.random.normal(0,1,(1,img_size,img_size,3)).astype(np.float32)
 model_keras = tf.keras.models.load_model('./model/unet_{}.h5'.format(img_size))
 
@@ -17,11 +18,11 @@ model_keras = tf.keras.models.load_model('./model/unet_{}.h5'.format(img_size))
 input_data = input_data.transpose([0, 3, 1, 2])
 shape_dict = {"input_1": input_data.shape}
 mod, params = relay.frontend.from_keras(model_keras, shape_dict)
-#target = 'cuda'
-target = 'llvm'
+target = 'cuda'
+#target = 'llvm'
 
-#dev = tvm.cuda()
-dev = tvm.cpu()
+dev = tvm.cuda()
+#dev = tvm.cpu()
 with tvm.transform.PassContext(opt_level=2):
     lib = relay.build(mod, target, params=params)
 
