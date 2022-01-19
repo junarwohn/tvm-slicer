9f264c1a1059fa0e0b4e281d64f87485e27d02db
Joonho Wohn <givenarc@gmail.com>
2022-01-19 17:00:43 +0900

    [neurosurgeon] add autoscript for server and client

--- tests/Unet/neurosurgeon/server/ex.sh
+++ tests/Unet/neurosurgeon/server/ex.sh
@@ -1,1 +1,3 @@
-python3 server.py  --target=cuda --ip=127.0.0.1 --ntp_enable=0 --partition_point=4 --model=unet --opt_level=3
+#python3 server.py  --target=cuda --ip=127.0.0.1 --ntp_enable=0 --partition_point=20 --model=unet --opt_level=3
+
+bash server.sh -r 30 -f graph_candidate.txt -i 512 -m unet -t cuda -o 3 -a 127.0.0.1 -n 0
