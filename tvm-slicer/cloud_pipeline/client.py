9b76b4c7d784aaed01fd0e3645ba7ab3203b944b
Joonho Wohn <givenarc@gmail.com>
2022-02-15 22:25:59 +0900

    [feature/pipeline] add pipeline logic

--- tvm-slicer/cloud_pipeline/client.py
+++ tvm-slicer/cloud_pipeline/client.py
@@ -15,7 +15,7 @@ import cv2
 import struct
 from argparse import ArgumentParser
 import ntplib 
-from multiprocessing import Process
+from multiprocessing import Process, Queue
 
 ntp_time_server = 'time.windows.com'               # NTP Server Domain Or IP 
 ntp_time_server = 'time.google.com'               # NTP Server Domain Or IP 
@@ -94,7 +94,7 @@ org=(50,100)
 font=cv2.FONT_HERSHEY_SIMPLEX
 
 
-def generate_img():
+def generate_img(q):
     cap = cv2.VideoCapture("../src/data/j_scan.mp4")
     timer_exclude_network = 0
     while (cap.isOpened()):
@@ -110,7 +110,7 @@ def generate_img():
         timer_exclude_network += time.time() - timer_exclude_network_start
         input_data = np.expand_dims(frame, 0).transpose([0, 3, 1, 2])
         msg_body = input_data.tobytes()
-
+        q.put(frame)
         total_send_msg_size = len(msg_body)
         send_msg = struct.pack('i', total_send_msg_size) + msg_body
         # Send object
@@ -120,7 +120,7 @@ def generate_img():
     print("Exclude network time :", timer_exclude_network)
 
     
-def recv_img():
+def recv_img(q):
     recv_msg = b''
     while True:
         while len(recv_msg) < 4:
@@ -140,18 +140,22 @@ def recv_img():
         out = np.frombuffer(recv_msg[:4*b*c*h*w], np.float32).reshape(tuple(final_output_shape))
         th = cv2.resize(cv2.threshold(np.squeeze(out.transpose([0,2,3,1])), 0.5, 1, cv2.THRESH_BINARY)[-1], (img_size,img_size))
         # img_in_rgb = frame
+        img_in_rgb = q.get()
+        img_in_rgb[th == 1] = [0, 0, 255]
+        cv2.imshow("received - client", img_in_rgb)
         # cv2.imshow("received - client", 255 * th)
         # cv2.waitKey(1)
-        # if cv2.waitKey(1) & 0xFF == ord('q'):
-            # break
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
         recv_msg = recv_msg[4*b*c*h*w:]
 
     cv2.destroyAllWindows()
 
 if __name__ == '__main__':
     timer_total_time = time.time()
-    p1 = Process(target=generate_img)
-    p2 = Process(target=recv_img)
+    q = Queue()
+    p1 = Process(target=generate_img, args=(q,))
+    p2 = Process(target=recv_img, args=(q,))
     p1.start(); 
     p2.start(); 
     p1.join(); p2.join()
--- tvm-slicer/multiprocessing/client.py
+++ tvm-slicer/multiprocessing/client.py
@@ -15,7 +15,7 @@ import cv2
 import struct
 from argparse import ArgumentParser
 import ntplib 
-from multiprocessing import Process, Queue
+from multiprocessing import Process, Queue, Lock
 
 ntp_time_server = 'time.windows.com'               # NTP Server Domain Or IP 
 ntp_time_server = 'time.google.com'               # NTP Server Domain Or IP 
@@ -76,7 +76,7 @@ with open(model_info_path, "r") as json_file:
 input_info = model_info["extra"]["inputs"]
 shape_info = model_info["attrs"]["shape"][1][:len(input_info)]
 output_info = model_info["extra"]["outputs"]
-print(shape_info)
+
 # Video Load
 img_size = 512 
 
@@ -102,8 +102,7 @@ print(final_output_shape)
 org=(50,100)
 font=cv2.FONT_HERSHEY_SIMPLEX
 
-
-def generate_img(q):
+def read_and_infernece(visual_queue, visual_lock, send_queue, send_lock):
     model_path = "../src/model/{}_{}_front_{}_{}_{}.so".format(args.model, args.target, args.img_size, args.opt_level, args.partition_point)
     front_lib = tvm.runtime.load_module(model_path)
     front_model = graph_executor.GraphModule(front_lib['default'](dev))
@@ -113,76 +112,159 @@ def generate_img(q):
         ret, frame = cap.read()
         try:
             frame = preprocess(frame)
+        # no more frame to read
         except:
-            total_msg = struct.pack('i', 0)
-            client_socket.sendall(total_msg)
-            client_socket.close()
+            # visual_lock.acquire()
+            visual_queue.put([])
+            # visual_lock.release()
+            # send_lock.acquire()
+            send_queue.put([])
+            # send_lock.release()
+            print('read_and_infernece end')
             break
-        # print("imread")
+
+        # visual_lock.acquire()
+        visual_queue.put(frame)
+        # visual_lock.release()
         input_data = np.expand_dims(frame, 0).transpose([0, 3, 1, 2])
         front_model.set_input("input_0", input_data)
         front_model.run()
+        # send_lock.acquire()
+        
         outs = []
         for i, out_idx in enumerate(output_info):
             out = front_model.get_output(i).asnumpy().astype(np.float32)
             outs.append(out)
-        # print("model run")
         
-        msg_body = b''
-        # Send msg
-        for out in outs:
-            out_byte = out.tobytes()
-            msg_body += out_byte
-        q.put(frame)
-        total_send_msg_size = len(msg_body)
-        send_msg = struct.pack('i', total_send_msg_size) + msg_body
-        # Send object
-        client_socket.sendall(send_msg)
-        # print("send")
-    client_socket.close()
+        # outs = []
+        # for i, out_idx in enumerate(output_info):
+        #     out = front_model.get_output(i).asnumpy().astype(np.float32)
+        
+        send_queue.put(outs)
+        # send_lock.release()
+        # print("read_and_infernece")
+    cap.release()
 
-    
-def recv_img(q):
+
+def send_data(send_queue, send_lock):
+    # Send msg
+    output_num = len(output_info)
+    while True:
+        if send_queue.qsize() != 0:
+            # outs = []
+            # # send_lock.acquire()
+            # for i in range(output_num):
+            #     outs.append(send_queue.get())
+            # send_lock.release()
+            outs = send_queue.get()
+            # inference end
+            if len(outs) == 0:
+                total_msg = struct.pack('i', 0)
+                client_socket.sendall(total_msg)
+                while send_queue.qsize() != 0:
+                    send_queue.get()
+                send_queue.close()
+                print("send_data end")
+                break
+
+            msg_body = b''
+            for out in outs:
+                out_byte = out.tobytes()
+                msg_body += out_byte
+
+            total_send_msg_size = len(msg_body)
+            send_msg = struct.pack('i', total_send_msg_size) + msg_body
+            client_socket.sendall(send_msg)
+            # print("send_data")
+
+        else:
+            # yield to other process
+            time.sleep(0)
+    # send_queue.close()
+
+
+def recv_data(result_queue, result_lock):
     recv_msg = b''
     while True:
         while len(recv_msg) < 4:
-            # print("recv")
             recv_msg += client_socket.recv(4)
         total_recv_msg_size = struct.unpack('i', recv_msg[:4])[0]
         recv_msg = recv_msg[4:]
+        
         if total_recv_msg_size == 0:
+            # result_lock.acquire()
+            print("recv end")
+            result_queue.put([])
+            # result_lock.release()
             break 
-        # print("total_recv_msg_size", total_recv_msg_size)
-        # recv_msg += client_socket.recv(total_recv_msg_size)
+
         while len(recv_msg) < total_recv_msg_size:
-            # print(len(recv_msg))
             recv_msg += client_socket.recv(total_recv_msg_size)
-        # img = np.frombuffer(recv_msg[:4*512*512*3], np.float32).reshape((512,512,3))
 
         b,c,h,w = final_output_shape
-        ## TODO : get output and parse 
         out = np.frombuffer(recv_msg[:4*b*c*h*w], np.float32).reshape(tuple(final_output_shape))
-        img_in_rgb = q.get()
-        th = cv2.resize(cv2.threshold(np.squeeze(out.transpose([0,2,3,1])), 0.5, 1, cv2.THRESH_BINARY)[-1], (img_size,img_size))
-        img_in_rgb[th == 1] = [0, 0, 255]
+        th = cv2.resize(cv2.threshold(np.squeeze(out.transpose([0,2,3,1])), 0.8, 1, cv2.THRESH_BINARY)[-1], (img_size,img_size))
 
-        cv2.imshow("received - client", img_in_rgb)
-        # cv2.imshow("received - client", 255 * th)
-        # # print(th)
-        # cv2.waitKey(1)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
+        # result_lock.acquire()
+        result_queue.put(th)
+        # result_lock.release()
+
         recv_msg = recv_msg[4*b*c*h*w:]
 
+def visualize(visual_queue, visual_lock, result_queue, result_lock):
+    cnt = 0
+    while True:
+        if result_queue.qsize() != 0:
+            # result_lock.acquire()
+            th = result_queue.get()
+            # result_lock.release()
+            # visual_lock.acquire()
+            img_in_rgb = visual_queue.get()
+            # visual_lock.release()
+            if len(img_in_rgb) == 0 or len(th) == 0:
+                while result_queue.qsize() != 0:
+                    result_queue.get()
+                while visual_queue.qsize() != 0:
+                    visual_queue.get()
+                print("visualize end")
+                break
+            img_in_rgb[th == 1] = [0, 0, 255]
+            cv2.imshow("received - client", img_in_rgb)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+            # print("visualize")
+        else:
+            time.sleep(0)
+    # visual_queue.close()
+    # result_queue.close()
     cv2.destroyAllWindows()
 
 if __name__ == '__main__':
-    q = Queue()
-    p1 = Process(target=generate_img, args=(q,))
-    p2 = Process(target=recv_img, args=(q,))
-    p1.start(); 
-    p2.start(); 
-    stime = time.time()
-    p1.join(); p2.join()
-    print(time.time() - stime)
 
+    visual_queue = Queue()
+    visual_lock = Lock()
+    send_queue = Queue()
+    send_lock = Lock() 
+    result_queue = Queue()
+    result_lock = Lock()
+
+    p_read_and_infernece = Process(target=read_and_infernece, args=(visual_queue, visual_lock, send_queue, send_lock))
+    p_send_data = Process(target=send_data, args=(send_queue, send_lock))
+    p_recv_data = Process(target=recv_data, args=(result_queue, result_lock))
+    p_visualize = Process(target=visualize, args=(visual_queue, visual_lock, result_queue, result_lock))
+
+    p_read_and_infernece.start()
+    p_send_data.start()
+    p_recv_data.start()
+    p_visualize.start()
+    stime = time.time()
+    p_read_and_infernece.join()
+    print("join p_read_and_infernece")
+    p_send_data.join()
+    print("join p_send_data")
+    p_recv_data.join()
+    print("join p_recv_data")
+    p_visualize.join()
+    print("join p_visualize")
+    print(time.time() - stime)
+    client_socket.close()
