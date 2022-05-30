0ae23d24fa91306cfcf338bd3ec7b37de1e1a8a3
Joonho Wohn <givenarc@gmail.com>
2022-05-30 13:21:59 +0900

    [nurburgring] add files

--- tests/nurburgring/pipeline/cmd_server_128.sh
+++ tests/nurburgring/pipeline/cmd_server_128.sh
@@ -1,6 +1,6 @@
 rm client_log.txt
 rm server_log.txt
-echo $(date)
+echo $(date) >> server_log.txt
 python3 server_pipeline_disabled.py -m unet -o 3 -i 128 -t cuda --ip 192.168.0.184 -p 9 115
 python3 server_pipeline_disabled.py -m unet -o 3 -i 128 -t cuda --ip 192.168.0.184 -p 9 115 >> server_log.txt
 python3 server_pipeline_disabled.py -m unet -o 3 -i 128 -t cuda --ip 192.168.0.184 -p 20 115
