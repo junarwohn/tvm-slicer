5f45e2e92027dd6b3426a4bae8b802f10274a43e
Joonho Wohn <givenarc@gmail.com>
2022-05-30 13:14:49 +0900

    [nurburgring] hot fix typo

--- tests/nurburgring/pipeline/cmd_server_128.sh
+++ tests/nurburgring/pipeline/cmd_server_128.sh
@@ -1,30 +1,30 @@
-python3 server_pipeline_diasbled.py -m unet -o 3 -i 128 -t cuda --ip 192.168.0.184 -p 9 115
-python3 server_pipeline_diasbled.py -m unet -o 3 -i 128 -t cuda --ip 192.168.0.184 -p 9 115 >> server_log.txt
-python3 server_pipeline_diasbled.py -m unet -o 3 -i 128 -t cuda --ip 192.168.0.184 -p 20 115
-python3 server_pipeline_diasbled.py -m unet -o 3 -i 128 -t cuda --ip 192.168.0.184 -p 20 115 >> server_log.txt
-python3 server_pipeline_diasbled.py -m unet -o 3 -i 128 -t cuda --ip 192.168.0.184 -p 31 115
-python3 server_pipeline_diasbled.py -m unet -o 3 -i 128 -t cuda --ip 192.168.0.184 -p 31 115 >> server_log.txt
-python3 server_pipeline_diasbled.py -m unet -o 3 -i 128 -t cuda --ip 192.168.0.184 -p 42 115
-python3 server_pipeline_diasbled.py -m unet -o 3 -i 128 -t cuda --ip 192.168.0.184 -p 42 115 >> server_log.txt
-python3 server_pipeline_diasbled.py -m unet -o 3 -i 128 -t cuda --ip 192.168.0.184 -p 42 115
-python3 server_pipeline_diasbled.py -m unet -o 3 -i 128 -t cuda --ip 192.168.0.184 -p 42 115 >> server_log.txt
-python3 server_pipeline_diasbled.py -m unet -o 3 -i 128 -t cuda --ip 192.168.0.184 -p 31 115
-python3 server_pipeline_diasbled.py -m unet -o 3 -i 128 -t cuda --ip 192.168.0.184 -p 31 115 >> server_log.txt
-python3 server_pipeline_diasbled.py -m unet -o 3 -i 128 -t cuda --ip 192.168.0.184 -p 42 115
-python3 server_pipeline_diasbled.py -m unet -o 3 -i 128 -t cuda --ip 192.168.0.184 -p 42 115 >> server_log.txt
-python3 server_pipeline_diasbled.py -m unet -o 3 -i 128 -t cuda --ip 192.168.0.184 -p 42 115
-python3 server_pipeline_diasbled.py -m unet -o 3 -i 128 -t cuda --ip 192.168.0.184 -p 42 115 >> server_log.txt
-python3 server_pipeline_diasbled.py -m unet -o 3 -i 128 -t cuda --ip 192.168.0.184 -p 20 115
-python3 server_pipeline_diasbled.py -m unet -o 3 -i 128 -t cuda --ip 192.168.0.184 -p 20 115 >> server_log.txt
-python3 server_pipeline_diasbled.py -m unet -o 3 -i 128 -t cuda --ip 192.168.0.184 -p 31 115
-python3 server_pipeline_diasbled.py -m unet -o 3 -i 128 -t cuda --ip 192.168.0.184 -p 31 115 >> server_log.txt
-python3 server_pipeline_diasbled.py -m unet -o 3 -i 128 -t cuda --ip 192.168.0.184 -p 42 115
-python3 server_pipeline_diasbled.py -m unet -o 3 -i 128 -t cuda --ip 192.168.0.184 -p 42 115 >> server_log.txt
-python3 server_pipeline_diasbled.py -m unet -o 3 -i 128 -t cuda --ip 192.168.0.184 -p 42 115
-python3 server_pipeline_diasbled.py -m unet -o 3 -i 128 -t cuda --ip 192.168.0.184 -p 42 115 >> server_log.txt
-python3 server_pipeline_diasbled.py -m unet -o 3 -i 128 -t cuda --ip 192.168.0.184 -p 31 115
-python3 server_pipeline_diasbled.py -m unet -o 3 -i 128 -t cuda --ip 192.168.0.184 -p 31 115 >> server_log.txt
-python3 server_pipeline_diasbled.py -m unet -o 3 -i 128 -t cuda --ip 192.168.0.184 -p 42 115
-python3 server_pipeline_diasbled.py -m unet -o 3 -i 128 -t cuda --ip 192.168.0.184 -p 42 115 >> server_log.txt
-python3 server_pipeline_diasbled.py -m unet -o 3 -i 128 -t cuda --ip 192.168.0.184 -p 42 115
-python3 server_pipeline_diasbled.py -m unet -o 3 -i 128 -t cuda --ip 192.168.0.184 -p 42 115 >> server_log.txt
+python3 server_pipeline_disabled.py -m unet -o 3 -i 128 -t cuda --ip 192.168.0.184 -p 9 115
+python3 server_pipeline_disabled.py -m unet -o 3 -i 128 -t cuda --ip 192.168.0.184 -p 9 115 >> server_log.txt
+python3 server_pipeline_disabled.py -m unet -o 3 -i 128 -t cuda --ip 192.168.0.184 -p 20 115
+python3 server_pipeline_disabled.py -m unet -o 3 -i 128 -t cuda --ip 192.168.0.184 -p 20 115 >> server_log.txt
+python3 server_pipeline_disabled.py -m unet -o 3 -i 128 -t cuda --ip 192.168.0.184 -p 31 115
+python3 server_pipeline_disabled.py -m unet -o 3 -i 128 -t cuda --ip 192.168.0.184 -p 31 115 >> server_log.txt
+python3 server_pipeline_disabled.py -m unet -o 3 -i 128 -t cuda --ip 192.168.0.184 -p 42 115
+python3 server_pipeline_disabled.py -m unet -o 3 -i 128 -t cuda --ip 192.168.0.184 -p 42 115 >> server_log.txt
+python3 server_pipeline_disabled.py -m unet -o 3 -i 128 -t cuda --ip 192.168.0.184 -p 42 115
+python3 server_pipeline_disabled.py -m unet -o 3 -i 128 -t cuda --ip 192.168.0.184 -p 42 115 >> server_log.txt
+python3 server_pipeline_disabled.py -m unet -o 3 -i 128 -t cuda --ip 192.168.0.184 -p 31 115
+python3 server_pipeline_disabled.py -m unet -o 3 -i 128 -t cuda --ip 192.168.0.184 -p 31 115 >> server_log.txt
+python3 server_pipeline_disabled.py -m unet -o 3 -i 128 -t cuda --ip 192.168.0.184 -p 42 115
+python3 server_pipeline_disabled.py -m unet -o 3 -i 128 -t cuda --ip 192.168.0.184 -p 42 115 >> server_log.txt
+python3 server_pipeline_disabled.py -m unet -o 3 -i 128 -t cuda --ip 192.168.0.184 -p 42 115
+python3 server_pipeline_disabled.py -m unet -o 3 -i 128 -t cuda --ip 192.168.0.184 -p 42 115 >> server_log.txt
+python3 server_pipeline_disabled.py -m unet -o 3 -i 128 -t cuda --ip 192.168.0.184 -p 20 115
+python3 server_pipeline_disabled.py -m unet -o 3 -i 128 -t cuda --ip 192.168.0.184 -p 20 115 >> server_log.txt
+python3 server_pipeline_disabled.py -m unet -o 3 -i 128 -t cuda --ip 192.168.0.184 -p 31 115
+python3 server_pipeline_disabled.py -m unet -o 3 -i 128 -t cuda --ip 192.168.0.184 -p 31 115 >> server_log.txt
+python3 server_pipeline_disabled.py -m unet -o 3 -i 128 -t cuda --ip 192.168.0.184 -p 42 115
+python3 server_pipeline_disabled.py -m unet -o 3 -i 128 -t cuda --ip 192.168.0.184 -p 42 115 >> server_log.txt
+python3 server_pipeline_disabled.py -m unet -o 3 -i 128 -t cuda --ip 192.168.0.184 -p 42 115
+python3 server_pipeline_disabled.py -m unet -o 3 -i 128 -t cuda --ip 192.168.0.184 -p 42 115 >> server_log.txt
+python3 server_pipeline_disabled.py -m unet -o 3 -i 128 -t cuda --ip 192.168.0.184 -p 31 115
+python3 server_pipeline_disabled.py -m unet -o 3 -i 128 -t cuda --ip 192.168.0.184 -p 31 115 >> server_log.txt
+python3 server_pipeline_disabled.py -m unet -o 3 -i 128 -t cuda --ip 192.168.0.184 -p 42 115
+python3 server_pipeline_disabled.py -m unet -o 3 -i 128 -t cuda --ip 192.168.0.184 -p 42 115 >> server_log.txt
+python3 server_pipeline_disabled.py -m unet -o 3 -i 128 -t cuda --ip 192.168.0.184 -p 42 115
+python3 server_pipeline_disabled.py -m unet -o 3 -i 128 -t cuda --ip 192.168.0.184 -p 42 115 >> server_log.txt
