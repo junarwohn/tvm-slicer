5fc6098e3023a85ee0cb32cc4ee7905ae9497594
Joonho Wohn <givenarc@gmail.com>
2022-07-05 19:07:46 +0900

    [nurburgring] fix graph error

--- tests/nurburgring/pipeline/cmd_client_512_59_pip.sh
+++ tests/nurburgring/pipeline/cmd_client_512_59_pip.sh
@@ -5,25 +5,25 @@ python3 client_pipeline_enabled.py -m unet -o 3 -i 512 -t cuda --ip 192.168.0.18
 sleep 5
 python3 client_pipeline_enabled.py -m unet -o 3 -i 512 -t cuda --ip 192.168.0.184 -p 0 10 59 >> client_log.txt
 sleep 5
-python3 client_pipeline_enabled.py -m unet -o 3 -i 512 -t cuda --ip 192.168.0.184 -p 0 10 21 59
+python3 client_pipeline_enabled.py -m unet -o 3 -i 512 -t cuda --ip 192.168.0.184 -p 0 10 20 59
 sleep 5
-python3 client_pipeline_enabled.py -m unet -o 3 -i 512 -t cuda --ip 192.168.0.184 -p 0 10 21 59 >> client_log.txt
+python3 client_pipeline_enabled.py -m unet -o 3 -i 512 -t cuda --ip 192.168.0.184 -p 0 10 20 59 >> client_log.txt
 sleep 5
-python3 client_pipeline_enabled.py -m unet -o 3 -i 512 -t cuda --ip 192.168.0.184 -p 0 10 21 31 59
+python3 client_pipeline_enabled.py -m unet -o 3 -i 512 -t cuda --ip 192.168.0.184 -p 0 10 20 31 59
 sleep 5
-python3 client_pipeline_enabled.py -m unet -o 3 -i 512 -t cuda --ip 192.168.0.184 -p 0 10 21 31 59 >> client_log.txt
+python3 client_pipeline_enabled.py -m unet -o 3 -i 512 -t cuda --ip 192.168.0.184 -p 0 10 20 31 59 >> client_log.txt
 sleep 5
 python3 client_pipeline_enabled.py -m unet -o 3 -i 512 -t cuda --ip 192.168.0.184 -p 0 10 31 59
 sleep 5
 python3 client_pipeline_enabled.py -m unet -o 3 -i 512 -t cuda --ip 192.168.0.184 -p 0 10 31 59 >> client_log.txt
 sleep 5
-python3 client_pipeline_enabled.py -m unet -o 3 -i 512 -t cuda --ip 192.168.0.184 -p 0 21 59
+python3 client_pipeline_enabled.py -m unet -o 3 -i 512 -t cuda --ip 192.168.0.184 -p 0 20 59
 sleep 5
-python3 client_pipeline_enabled.py -m unet -o 3 -i 512 -t cuda --ip 192.168.0.184 -p 0 21 59 >> client_log.txt
+python3 client_pipeline_enabled.py -m unet -o 3 -i 512 -t cuda --ip 192.168.0.184 -p 0 20 59 >> client_log.txt
 sleep 5
-python3 client_pipeline_enabled.py -m unet -o 3 -i 512 -t cuda --ip 192.168.0.184 -p 0 21 31 59
+python3 client_pipeline_enabled.py -m unet -o 3 -i 512 -t cuda --ip 192.168.0.184 -p 0 20 31 59
 sleep 5
-python3 client_pipeline_enabled.py -m unet -o 3 -i 512 -t cuda --ip 192.168.0.184 -p 0 21 31 59 >> client_log.txt
+python3 client_pipeline_enabled.py -m unet -o 3 -i 512 -t cuda --ip 192.168.0.184 -p 0 20 31 59 >> client_log.txt
 sleep 5
 python3 client_pipeline_enabled.py -m unet -o 3 -i 512 -t cuda --ip 192.168.0.184 -p 0 31 59
 sleep 5
