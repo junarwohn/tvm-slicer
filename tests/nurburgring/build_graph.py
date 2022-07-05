22b2f040a712824bca76f084343e65da1a28bed8
Joonho Wohn <givenarc@gmail.com>
2022-07-05 15:47:14 +0900

    [nurburgring] add files

--- tests/nurburgring/build_graph.py
+++ tests/nurburgring/build_graph.py
@@ -23,10 +23,12 @@ class UnetPreProcessCallback(DFPatternCallback):
         concat_node = is_op('concatenate')(tuple_node)
         self.pattern = concat_node
         self.match_node = []
+        self.match_node2 = []
 
     def callback(self, pre, post, node_map):
         var2 = node_map[self.var2][0]
         self.match_node.append(var2)
+        self.match_node2.append(pre)
         return post
 
 class UnetCallback(DFPatternCallback):
@@ -70,6 +72,52 @@ class UnetCallback(DFPatternCallback):
                 return self.dequant(self.quant(post))
         return post
 
+
+class UnetCallback2(DFPatternCallback):
+    # A callback class to rewrite the matched pattern to a batch_norm op.
+    def __init__(self, match_node, require_type=False):
+        super().__init__(require_type)
+        super().__init__(rewrite_once=True)
+
+        # self.tuple_get_item_node = is_tuple_get_item(wildcard(), 0)
+        # self.pattern_1 = self.tuple_get_item_node
+        self.var2 = wildcard()
+        tuple_node = is_tuple([wildcard(), self.var2])
+        concat_node = is_op('concatenate')(tuple_node)
+        self.pattern = concat_node
+        # self.pattern = self.pattern_1 
+        self.match_node = match_node
+        self.counter = 0
+        self.tmp = []
+
+    def quant(self, node):
+        cast_to_int8 = relay.cast(
+            relay.clip(
+                relay.round(
+                    relay.multiply(node, relay.const(8.0))
+                ), 
+                a_min=-127.0, a_max=127.0
+            ),
+            dtype="int8"
+        )
+        result_node = relay.annotation.stop_fusion(cast_to_int8)
+        self.tmp.append(result_node)
+        return result_node
+
+    def dequant(self, node):
+        cast_to_float32 = relay.divide(
+            relay.cast(node, dtype='float32'), relay.const(8.0)
+        )
+        return cast_to_float32
+
+    def callback(self, pre, post, node_map):
+        if self.pattern.match(pre):
+            if pre in self.match_node:
+                print("pat 1")
+                return self.dequant(self.quant(post))
+        return post
+
+
 parser = ArgumentParser()
 parser.add_argument('--start_point', '-s', type=int, default=0)
 parser.add_argument('--end_point', '-e', type=int, default=-1)
@@ -111,8 +159,12 @@ rewrite(upc, mod['main'])
 uc = UnetCallback(upc.match_node)
 out = rewrite(uc, mod['main'])
 
-out = relay.Function(out.params, relay.Tuple(uc.tmp + [out.body]), out.ret_type, out.type_params, out.attrs)
+upc = UnetPreProcessCallback()
+rewrite(upc, out)
+uc2 = UnetCallback2(upc.match_node2)
+out = rewrite(uc2, out)
 
+out = relay.Function(out.params, relay.Tuple(uc.tmp + [out.body]), out.ret_type, out.type_params, out.attrs)
 # out = mod
 
 print(out)
