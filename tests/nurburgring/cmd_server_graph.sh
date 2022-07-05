b4933af708e5e0efe916d8831252daea51eca3cd
Joonho Wohn <givenarc@gmail.com>
2022-07-05 21:26:59 +0900

    [nurburgring] add fffff

---
+++ tests/nurburgring/cmd_server_graph.sh
@@ -0,0 +1,15 @@
+python3 slicing_graph.py -m unet -o 3 -i 512 -t cuda  -p 0 9 119
+python3 slicing_graph.py -m unet -o 3 -i 512 -t cuda  -p 0 9 20 119
+python3 slicing_graph.py -m unet -o 3 -i 512 -t cuda  -p 0 9 20 31 119
+python3 slicing_graph.py -m unet -o 3 -i 512 -t cuda  -p 0 9 20 31 42 119
+python3 slicing_graph.py -m unet -o 3 -i 512 -t cuda  -p 0 9 20 42 119
+python3 slicing_graph.py -m unet -o 3 -i 512 -t cuda  -p 0 9 31 119
+python3 slicing_graph.py -m unet -o 3 -i 512 -t cuda  -p 0 9 31 42 119
+python3 slicing_graph.py -m unet -o 3 -i 512 -t cuda  -p 0 9 42 119
+python3 slicing_graph.py -m unet -o 3 -i 512 -t cuda  -p 0 20 119
+python3 slicing_graph.py -m unet -o 3 -i 512 -t cuda  -p 0 20 31 119
+python3 slicing_graph.py -m unet -o 3 -i 512 -t cuda  -p 0 20 31 42 119
+python3 slicing_graph.py -m unet -o 3 -i 512 -t cuda  -p 0 20 42 119
+python3 slicing_graph.py -m unet -o 3 -i 512 -t cuda  -p 0 31 119
+python3 slicing_graph.py -m unet -o 3 -i 512 -t cuda  -p 0 31 42 119
+python3 slicing_graph.py -m unet -o 3 -i 512 -t cuda  -p 0 42 119
--- tests/nurburgring/pipeline/cmd_server_512_pip.sh
+++ tests/nurburgring/pipeline/cmd_server_512_pip.sh
@@ -2,33 +2,33 @@ rm client_log.txt
 rm server_log.txt
 echo $(date) >> client_log.txt
 echo $(date) >> server_log.txt
-python3 server_pipeline_enabled.py -m unet -o 3 -i 512 -t cuda --ip 192.168.0.184 -p 9 115
-python3 server_pipeline_enabled.py -m unet -o 3 -i 512 -t cuda --ip 192.168.0.184 -p 9 115 >> server_log.txt
-python3 server_pipeline_enabled.py -m unet -o 3 -i 512 -t cuda --ip 192.168.0.184 -p 20 115
-python3 server_pipeline_enabled.py -m unet -o 3 -i 512 -t cuda --ip 192.168.0.184 -p 20 115 >> server_log.txt
-python3 server_pipeline_enabled.py -m unet -o 3 -i 512 -t cuda --ip 192.168.0.184 -p 31 115
-python3 server_pipeline_enabled.py -m unet -o 3 -i 512 -t cuda --ip 192.168.0.184 -p 31 115 >> server_log.txt
-python3 server_pipeline_enabled.py -m unet -o 3 -i 512 -t cuda --ip 192.168.0.184 -p 42 115
-python3 server_pipeline_enabled.py -m unet -o 3 -i 512 -t cuda --ip 192.168.0.184 -p 42 115 >> server_log.txt
-python3 server_pipeline_enabled.py -m unet -o 3 -i 512 -t cuda --ip 192.168.0.184 -p 42 115
-python3 server_pipeline_enabled.py -m unet -o 3 -i 512 -t cuda --ip 192.168.0.184 -p 42 115 >> server_log.txt
-python3 server_pipeline_enabled.py -m unet -o 3 -i 512 -t cuda --ip 192.168.0.184 -p 31 115
-python3 server_pipeline_enabled.py -m unet -o 3 -i 512 -t cuda --ip 192.168.0.184 -p 31 115 >> server_log.txt
-python3 server_pipeline_enabled.py -m unet -o 3 -i 512 -t cuda --ip 192.168.0.184 -p 42 115
-python3 server_pipeline_enabled.py -m unet -o 3 -i 512 -t cuda --ip 192.168.0.184 -p 42 115 >> server_log.txt
-python3 server_pipeline_enabled.py -m unet -o 3 -i 512 -t cuda --ip 192.168.0.184 -p 42 115
-python3 server_pipeline_enabled.py -m unet -o 3 -i 512 -t cuda --ip 192.168.0.184 -p 42 115 >> server_log.txt
-python3 server_pipeline_enabled.py -m unet -o 3 -i 512 -t cuda --ip 192.168.0.184 -p 20 115
-python3 server_pipeline_enabled.py -m unet -o 3 -i 512 -t cuda --ip 192.168.0.184 -p 20 115 >> server_log.txt
-python3 server_pipeline_enabled.py -m unet -o 3 -i 512 -t cuda --ip 192.168.0.184 -p 31 115
-python3 server_pipeline_enabled.py -m unet -o 3 -i 512 -t cuda --ip 192.168.0.184 -p 31 115 >> server_log.txt
-python3 server_pipeline_enabled.py -m unet -o 3 -i 512 -t cuda --ip 192.168.0.184 -p 42 115
-python3 server_pipeline_enabled.py -m unet -o 3 -i 512 -t cuda --ip 192.168.0.184 -p 42 115 >> server_log.txt
-python3 server_pipeline_enabled.py -m unet -o 3 -i 512 -t cuda --ip 192.168.0.184 -p 42 115
-python3 server_pipeline_enabled.py -m unet -o 3 -i 512 -t cuda --ip 192.168.0.184 -p 42 115 >> server_log.txt
-python3 server_pipeline_enabled.py -m unet -o 3 -i 512 -t cuda --ip 192.168.0.184 -p 31 115
-python3 server_pipeline_enabled.py -m unet -o 3 -i 512 -t cuda --ip 192.168.0.184 -p 31 115 >> server_log.txt
-python3 server_pipeline_enabled.py -m unet -o 3 -i 512 -t cuda --ip 192.168.0.184 -p 42 115
-python3 server_pipeline_enabled.py -m unet -o 3 -i 512 -t cuda --ip 192.168.0.184 -p 42 115 >> server_log.txt
-python3 server_pipeline_enabled.py -m unet -o 3 -i 512 -t cuda --ip 192.168.0.184 -p 42 115
-python3 server_pipeline_enabled.py -m unet -o 3 -i 512 -t cuda --ip 192.168.0.184 -p 42 115 >> server_log.txt
+python3 server_pipeline_enabled.py -m unet -o 3 -i 512 -t cuda --ip 192.168.0.184 -p 9 119
+python3 server_pipeline_enabled.py -m unet -o 3 -i 512 -t cuda --ip 192.168.0.184 -p 9 119 >> server_log.txt
+python3 server_pipeline_enabled.py -m unet -o 3 -i 512 -t cuda --ip 192.168.0.184 -p 20 119
+python3 server_pipeline_enabled.py -m unet -o 3 -i 512 -t cuda --ip 192.168.0.184 -p 20 119 >> server_log.txt
+python3 server_pipeline_enabled.py -m unet -o 3 -i 512 -t cuda --ip 192.168.0.184 -p 31 119
+python3 server_pipeline_enabled.py -m unet -o 3 -i 512 -t cuda --ip 192.168.0.184 -p 31 119 >> server_log.txt
+python3 server_pipeline_enabled.py -m unet -o 3 -i 512 -t cuda --ip 192.168.0.184 -p 42 119
+python3 server_pipeline_enabled.py -m unet -o 3 -i 512 -t cuda --ip 192.168.0.184 -p 42 119 >> server_log.txt
+python3 server_pipeline_enabled.py -m unet -o 3 -i 512 -t cuda --ip 192.168.0.184 -p 42 119
+python3 server_pipeline_enabled.py -m unet -o 3 -i 512 -t cuda --ip 192.168.0.184 -p 42 119 >> server_log.txt
+python3 server_pipeline_enabled.py -m unet -o 3 -i 512 -t cuda --ip 192.168.0.184 -p 31 119
+python3 server_pipeline_enabled.py -m unet -o 3 -i 512 -t cuda --ip 192.168.0.184 -p 31 119 >> server_log.txt
+python3 server_pipeline_enabled.py -m unet -o 3 -i 512 -t cuda --ip 192.168.0.184 -p 42 119
+python3 server_pipeline_enabled.py -m unet -o 3 -i 512 -t cuda --ip 192.168.0.184 -p 42 119 >> server_log.txt
+python3 server_pipeline_enabled.py -m unet -o 3 -i 512 -t cuda --ip 192.168.0.184 -p 42 119
+python3 server_pipeline_enabled.py -m unet -o 3 -i 512 -t cuda --ip 192.168.0.184 -p 42 119 >> server_log.txt
+python3 server_pipeline_enabled.py -m unet -o 3 -i 512 -t cuda --ip 192.168.0.184 -p 20 119
+python3 server_pipeline_enabled.py -m unet -o 3 -i 512 -t cuda --ip 192.168.0.184 -p 20 119 >> server_log.txt
+python3 server_pipeline_enabled.py -m unet -o 3 -i 512 -t cuda --ip 192.168.0.184 -p 31 119
+python3 server_pipeline_enabled.py -m unet -o 3 -i 512 -t cuda --ip 192.168.0.184 -p 31 119 >> server_log.txt
+python3 server_pipeline_enabled.py -m unet -o 3 -i 512 -t cuda --ip 192.168.0.184 -p 42 119
+python3 server_pipeline_enabled.py -m unet -o 3 -i 512 -t cuda --ip 192.168.0.184 -p 42 119 >> server_log.txt
+python3 server_pipeline_enabled.py -m unet -o 3 -i 512 -t cuda --ip 192.168.0.184 -p 42 119
+python3 server_pipeline_enabled.py -m unet -o 3 -i 512 -t cuda --ip 192.168.0.184 -p 42 119 >> server_log.txt
+python3 server_pipeline_enabled.py -m unet -o 3 -i 512 -t cuda --ip 192.168.0.184 -p 31 119
+python3 server_pipeline_enabled.py -m unet -o 3 -i 512 -t cuda --ip 192.168.0.184 -p 31 119 >> server_log.txt
+python3 server_pipeline_enabled.py -m unet -o 3 -i 512 -t cuda --ip 192.168.0.184 -p 42 119
+python3 server_pipeline_enabled.py -m unet -o 3 -i 512 -t cuda --ip 192.168.0.184 -p 42 119 >> server_log.txt
+python3 server_pipeline_enabled.py -m unet -o 3 -i 512 -t cuda --ip 192.168.0.184 -p 42 119
+python3 server_pipeline_enabled.py -m unet -o 3 -i 512 -t cuda --ip 192.168.0.184 -p 42 119 >> server_log.txt
