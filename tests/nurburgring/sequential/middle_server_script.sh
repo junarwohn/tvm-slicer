56ee77cf62a3167389a65866800cdd3e52ca779d
Joonho Wohn <givenarc@gmail.com>
2022-07-15 17:51:02 +0900

    add files

---
+++ tests/nurburgring/sequential/middle_server_script.sh
@@ -0,0 +1,11 @@
+rm server_log.txt
+
+echo $(date) >> server_log.txt
+
+# Sleep and Warm up
+python3 model_tester.py -p 0 141
+python3 middle_server.py -m unet -t cuda -o 3 -i 512 -p 13 115 --ip=192.168.0.184 >> server_log.txt
+
+# Sleep and Warm up
+python3 model_tester.py -p 0 141
+python3 middle_server.py -m unet -t cuda -o 3 -i 512 -p 33 97 --ip=192.168.0.184 >> server_log.txt
