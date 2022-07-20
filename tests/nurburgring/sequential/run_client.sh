48dc37a6bad23dc8e5f723d9aabb7c35c4655f8d
Joonho Wohn <givenarc@gmail.com>
2022-07-20 09:15:36 +0900

    fix scipts

--- tests/nurburgring/sequential/run_client.sh
+++ tests/nurburgring/sequential/run_client.sh
@@ -1185,201 +1185,201 @@ python3 client.py -f 0 127 -b 133 141 >> client_log.txt
 sleep 3
 python3 client.py -f 0 5 -b 141
 sleep 3
-python3 client.py -f 0 5 -b 141 >> server_log.txt
+python3 client.py -f 0 5 -b 141 >> client_log.txt
 sleep 3
-python3 server.py -f 0 -b 5 141
+python3 client.py -f 0 -b 5 141
 sleep 3
-python3 server.py -f 0 -b 5 141 >> server_log.txt
+python3 client.py -f 0 -b 5 141 >> client_log.txt
 sleep 3
 python3 client.py -f 0 11 -b 141
 sleep 3
-python3 client.py -f 0 11 -b 141 >> server_log.txt
+python3 client.py -f 0 11 -b 141 >> client_log.txt
 sleep 3
-python3 server.py -f 0 -b 11 141
+python3 client.py -f 0 -b 11 141
 sleep 3
-python3 server.py -f 0 -b 11 141 >> server_log.txt
+python3 client.py -f 0 -b 11 141 >> client_log.txt
 sleep 3
 python3 client.py -f 0 13 -b 141
 sleep 3
-python3 client.py -f 0 13 -b 141 >> server_log.txt
+python3 client.py -f 0 13 -b 141 >> client_log.txt
 sleep 3
-python3 server.py -f 0 -b 13 141
+python3 client.py -f 0 -b 13 141
 sleep 3
-python3 server.py -f 0 -b 13 141 >> server_log.txt
+python3 client.py -f 0 -b 13 141 >> client_log.txt
 sleep 3
 python3 client.py -f 0 19 -b 141
 sleep 3
-python3 client.py -f 0 19 -b 141 >> server_log.txt
+python3 client.py -f 0 19 -b 141 >> client_log.txt
 sleep 3
-python3 server.py -f 0 -b 19 141
+python3 client.py -f 0 -b 19 141
 sleep 3
-python3 server.py -f 0 -b 19 141 >> server_log.txt
+python3 client.py -f 0 -b 19 141 >> client_log.txt
 sleep 3
 python3 client.py -f 0 25 -b 141
 sleep 3
-python3 client.py -f 0 25 -b 141 >> server_log.txt
+python3 client.py -f 0 25 -b 141 >> client_log.txt
 sleep 3
-python3 server.py -f 0 -b 25 141
+python3 client.py -f 0 -b 25 141
 sleep 3
-python3 server.py -f 0 -b 25 141 >> server_log.txt
+python3 client.py -f 0 -b 25 141 >> client_log.txt
 sleep 3
 python3 client.py -f 0 27 -b 141
 sleep 3
-python3 client.py -f 0 27 -b 141 >> server_log.txt
+python3 client.py -f 0 27 -b 141 >> client_log.txt
 sleep 3
-python3 server.py -f 0 -b 27 141
+python3 client.py -f 0 -b 27 141
 sleep 3
-python3 server.py -f 0 -b 27 141 >> server_log.txt
+python3 client.py -f 0 -b 27 141 >> client_log.txt
 sleep 3
 python3 client.py -f 0 33 -b 141
 sleep 3
-python3 client.py -f 0 33 -b 141 >> server_log.txt
+python3 client.py -f 0 33 -b 141 >> client_log.txt
 sleep 3
-python3 server.py -f 0 -b 33 141
+python3 client.py -f 0 -b 33 141
 sleep 3
-python3 server.py -f 0 -b 33 141 >> server_log.txt
+python3 client.py -f 0 -b 33 141 >> client_log.txt
 sleep 3
 python3 client.py -f 0 39 -b 141
 sleep 3
-python3 client.py -f 0 39 -b 141 >> server_log.txt
+python3 client.py -f 0 39 -b 141 >> client_log.txt
 sleep 3
-python3 server.py -f 0 -b 39 141
+python3 client.py -f 0 -b 39 141
 sleep 3
-python3 server.py -f 0 -b 39 141 >> server_log.txt
+python3 client.py -f 0 -b 39 141 >> client_log.txt
 sleep 3
 python3 client.py -f 0 41 -b 141
 sleep 3
-python3 client.py -f 0 41 -b 141 >> server_log.txt
+python3 client.py -f 0 41 -b 141 >> client_log.txt
 sleep 3
-python3 server.py -f 0 -b 41 141
+python3 client.py -f 0 -b 41 141
 sleep 3
-python3 server.py -f 0 -b 41 141 >> server_log.txt
+python3 client.py -f 0 -b 41 141 >> client_log.txt
 sleep 3
 python3 client.py -f 0 47 -b 141
 sleep 3
-python3 client.py -f 0 47 -b 141 >> server_log.txt
+python3 client.py -f 0 47 -b 141 >> client_log.txt
 sleep 3
-python3 server.py -f 0 -b 47 141
+python3 client.py -f 0 -b 47 141
 sleep 3
-python3 server.py -f 0 -b 47 141 >> server_log.txt
+python3 client.py -f 0 -b 47 141 >> client_log.txt
 sleep 3
 python3 client.py -f 0 53 -b 141
 sleep 3
-python3 client.py -f 0 53 -b 141 >> server_log.txt
+python3 client.py -f 0 53 -b 141 >> client_log.txt
 sleep 3
-python3 server.py -f 0 -b 53 141
+python3 client.py -f 0 -b 53 141
 sleep 3
-python3 server.py -f 0 -b 53 141 >> server_log.txt
+python3 client.py -f 0 -b 53 141 >> client_log.txt
 sleep 3
 python3 client.py -f 0 55 -b 141
 sleep 3
-python3 client.py -f 0 55 -b 141 >> server_log.txt
+python3 client.py -f 0 55 -b 141 >> client_log.txt
 sleep 3
-python3 server.py -f 0 -b 55 141
+python3 client.py -f 0 -b 55 141
 sleep 3
-python3 server.py -f 0 -b 55 141 >> server_log.txt
+python3 client.py -f 0 -b 55 141 >> client_log.txt
 sleep 3
 python3 client.py -f 0 61 -b 141
 sleep 3
-python3 client.py -f 0 61 -b 141 >> server_log.txt
+python3 client.py -f 0 61 -b 141 >> client_log.txt
 sleep 3
-python3 server.py -f 0 -b 61 141
+python3 client.py -f 0 -b 61 141
 sleep 3
-python3 server.py -f 0 -b 61 141 >> server_log.txt
+python3 client.py -f 0 -b 61 141 >> client_log.txt
 sleep 3
 python3 client.py -f 0 72 -b 141
 sleep 3
-python3 client.py -f 0 72 -b 141 >> server_log.txt
+python3 client.py -f 0 72 -b 141 >> client_log.txt
 sleep 3
-python3 server.py -f 0 -b 72 141
+python3 client.py -f 0 -b 72 141
 sleep 3
-python3 server.py -f 0 -b 72 141 >> server_log.txt
+python3 client.py -f 0 -b 72 141 >> client_log.txt
 sleep 3
 python3 client.py -f 0 73 -b 141
 sleep 3
-python3 client.py -f 0 73 -b 141 >> server_log.txt
+python3 client.py -f 0 73 -b 141 >> client_log.txt
 sleep 3
-python3 server.py -f 0 -b 73 141
+python3 client.py -f 0 -b 73 141
 sleep 3
-python3 server.py -f 0 -b 73 141 >> server_log.txt
+python3 client.py -f 0 -b 73 141 >> client_log.txt
 sleep 3
 python3 client.py -f 0 79 -b 141
 sleep 3
-python3 client.py -f 0 79 -b 141 >> server_log.txt
+python3 client.py -f 0 79 -b 141 >> client_log.txt
 sleep 3
-python3 server.py -f 0 -b 79 141
+python3 client.py -f 0 -b 79 141
 sleep 3
-python3 server.py -f 0 -b 79 141 >> server_log.txt
+python3 client.py -f 0 -b 79 141 >> client_log.txt
 sleep 3
 python3 client.py -f 0 90 -b 141
 sleep 3
-python3 client.py -f 0 90 -b 141 >> server_log.txt
+python3 client.py -f 0 90 -b 141 >> client_log.txt
 sleep 3
-python3 server.py -f 0 -b 90 141
+python3 client.py -f 0 -b 90 141
 sleep 3
-python3 server.py -f 0 -b 90 141 >> server_log.txt
+python3 client.py -f 0 -b 90 141 >> client_log.txt
 sleep 3
 python3 client.py -f 0 91 -b 141
 sleep 3
-python3 client.py -f 0 91 -b 141 >> server_log.txt
+python3 client.py -f 0 91 -b 141 >> client_log.txt
 sleep 3
-python3 server.py -f 0 -b 91 141
+python3 client.py -f 0 -b 91 141
 sleep 3
-python3 server.py -f 0 -b 91 141 >> server_log.txt
+python3 client.py -f 0 -b 91 141 >> client_log.txt
 sleep 3
 python3 client.py -f 0 97 -b 141
 sleep 3
-python3 client.py -f 0 97 -b 141 >> server_log.txt
+python3 client.py -f 0 97 -b 141 >> client_log.txt
 sleep 3
-python3 server.py -f 0 -b 97 141
+python3 client.py -f 0 -b 97 141
 sleep 3
-python3 server.py -f 0 -b 97 141 >> server_log.txt
+python3 client.py -f 0 -b 97 141 >> client_log.txt
 sleep 3
 python3 client.py -f 0 108 -b 141
 sleep 3
-python3 client.py -f 0 108 -b 141 >> server_log.txt
+python3 client.py -f 0 108 -b 141 >> client_log.txt
 sleep 3
-python3 server.py -f 0 -b 108 141
+python3 client.py -f 0 -b 108 141
 sleep 3
-python3 server.py -f 0 -b 108 141 >> server_log.txt
+python3 client.py -f 0 -b 108 141 >> client_log.txt
 sleep 3
 python3 client.py -f 0 109 -b 141
 sleep 3
-python3 client.py -f 0 109 -b 141 >> server_log.txt
+python3 client.py -f 0 109 -b 141 >> client_log.txt
 sleep 3
-python3 server.py -f 0 -b 109 141
+python3 client.py -f 0 -b 109 141
 sleep 3
-python3 server.py -f 0 -b 109 141 >> server_log.txt
+python3 client.py -f 0 -b 109 141 >> client_log.txt
 sleep 3
 python3 client.py -f 0 115 -b 141
 sleep 3
-python3 client.py -f 0 115 -b 141 >> server_log.txt
+python3 client.py -f 0 115 -b 141 >> client_log.txt
 sleep 3
-python3 server.py -f 0 -b 115 141
+python3 client.py -f 0 -b 115 141
 sleep 3
-python3 server.py -f 0 -b 115 141 >> server_log.txt
+python3 client.py -f 0 -b 115 141 >> client_log.txt
 sleep 3
 python3 client.py -f 0 126 -b 141
 sleep 3
-python3 client.py -f 0 126 -b 141 >> server_log.txt
+python3 client.py -f 0 126 -b 141 >> client_log.txt
 sleep 3
-python3 server.py -f 0 -b 126 141
+python3 client.py -f 0 -b 126 141
 sleep 3
-python3 server.py -f 0 -b 126 141 >> server_log.txt
+python3 client.py -f 0 -b 126 141 >> client_log.txt
 sleep 3
 python3 client.py -f 0 127 -b 141
 sleep 3
-python3 client.py -f 0 127 -b 141 >> server_log.txt
+python3 client.py -f 0 127 -b 141 >> client_log.txt
 sleep 3
-python3 server.py -f 0 -b 127 141
+python3 client.py -f 0 -b 127 141
 sleep 3
-python3 server.py -f 0 -b 127 141 >> server_log.txt
+python3 client.py -f 0 -b 127 141 >> client_log.txt
 sleep 3
 python3 client.py -f 0 133 -b 141
 sleep 3
-python3 client.py -f 0 133 -b 141 >> server_log.txt
+python3 client.py -f 0 133 -b 141 >> client_log.txt
 sleep 3
-python3 server.py -f 0 -b 133 141
+python3 client.py -f 0 -b 133 141
 sleep 3
-python3 server.py -f 0 -b 133 141 >> server_log.txt
+python3 client.py -f 0 -b 133 141 >> client_log.txt
 sleep 3
