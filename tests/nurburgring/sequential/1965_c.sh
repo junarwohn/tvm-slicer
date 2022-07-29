364fcbec04df9bbddba1d755df7d5a3751f7c922
Joonho Wohn <givenarc@gmail.com>
2022-07-29 15:10:00 +0900

    add files

--- tests/nurburgring/sequential/1965_c.sh
+++ tests/nurburgring/sequential/1965_c.sh
@@ -1,218 +1,218 @@
 rm client_log.txt
-python3 client.py -f 0  -b 141 
+python3 client_lathide.py -f 0  -b 141 
 sleep 3
-python3 client.py -f 0  -b 141  >> client_log.txt
+python3 client_lathide.py -f 0  -b 141  >> client_log.txt
 sleep 3
-python3 client.py -f 0 11 -b 141 
+python3 client_lathide.py -f 0 11 -b 141 
 sleep 3
-python3 client.py -f 0 11 -b 141  >> client_log.txt
+python3 client_lathide.py -f 0 11 -b 141  >> client_log.txt
 sleep 3
-python3 client.py -f 0 5 -b 141 
+python3 client_lathide.py -f 0 5 -b 141 
 sleep 3
-python3 client.py -f 0 5 -b 141  >> client_log.txt
+python3 client_lathide.py -f 0 5 -b 141  >> client_log.txt
 sleep 3
-python3 client.py -f 0 13 -b 141 
+python3 client_lathide.py -f 0 13 -b 141 
 sleep 3
-python3 client.py -f 0 13 -b 141  >> client_log.txt
+python3 client_lathide.py -f 0 13 -b 141  >> client_log.txt
 sleep 3
-python3 client.py -f 0 13 -b 126 141
+python3 client_lathide.py -f 0 13 -b 126 141
 sleep 3
-python3 client.py -f 0 13 -b 126 141 >> client_log.txt
+python3 client_lathide.py -f 0 13 -b 126 141 >> client_log.txt
 sleep 3
-python3 client.py -f 0  -b 133 141
+python3 client_lathide.py -f 0  -b 133 141
 sleep 3
-python3 client.py -f 0  -b 133 141 >> client_log.txt
+python3 client_lathide.py -f 0  -b 133 141 >> client_log.txt
 sleep 3
-python3 client.py -f 0 25 -b 141 
+python3 client_lathide.py -f 0 25 -b 141 
 sleep 3
-python3 client.py -f 0 25 -b 141  >> client_log.txt
+python3 client_lathide.py -f 0 25 -b 141  >> client_log.txt
 sleep 3
-python3 client.py -f 0 19 -b 141 
+python3 client_lathide.py -f 0 19 -b 141 
 sleep 3
-python3 client.py -f 0 19 -b 141  >> client_log.txt
+python3 client_lathide.py -f 0 19 -b 141  >> client_log.txt
 sleep 3
-python3 client.py -f 0 115 -b 141 
+python3 client_lathide.py -f 0 115 -b 141 
 sleep 3
-python3 client.py -f 0 115 -b 141  >> client_log.txt
+python3 client_lathide.py -f 0 115 -b 141  >> client_log.txt
 sleep 3
-python3 client.py -f 0 27 -b 141 
+python3 client_lathide.py -f 0 27 -b 141 
 sleep 3
-python3 client.py -f 0 27 -b 141  >> client_log.txt
+python3 client_lathide.py -f 0 27 -b 141  >> client_log.txt
 sleep 3
-python3 client.py -f 0 97 -b 141 
+python3 client_lathide.py -f 0 97 -b 141 
 sleep 3
-python3 client.py -f 0 97 -b 141  >> client_log.txt
+python3 client_lathide.py -f 0 97 -b 141  >> client_log.txt
 sleep 3
-python3 client.py -f 0 25 -b 126 141
+python3 client_lathide.py -f 0 25 -b 126 141
 sleep 3
-python3 client.py -f 0 25 -b 126 141 >> client_log.txt
+python3 client_lathide.py -f 0 25 -b 126 141 >> client_log.txt
 sleep 3
-python3 client.py -f 0 19 -b 126 141
+python3 client_lathide.py -f 0 19 -b 126 141
 sleep 3
-python3 client.py -f 0 19 -b 126 141 >> client_log.txt
+python3 client_lathide.py -f 0 19 -b 126 141 >> client_log.txt
 sleep 3
-python3 client.py -f 0 39 -b 141 
+python3 client_lathide.py -f 0 39 -b 141 
 sleep 3
-python3 client.py -f 0 39 -b 141  >> client_log.txt
+python3 client_lathide.py -f 0 39 -b 141  >> client_log.txt
 sleep 3
-python3 client.py -f 0 33 -b 141 
+python3 client_lathide.py -f 0 33 -b 141 
 sleep 3
-python3 client.py -f 0 33 -b 141  >> client_log.txt
+python3 client_lathide.py -f 0 33 -b 141  >> client_log.txt
 sleep 3
-python3 client.py -f 0 27 -b 126 141
+python3 client_lathide.py -f 0 27 -b 126 141
 sleep 3
-python3 client.py -f 0 27 -b 126 141 >> client_log.txt
+python3 client_lathide.py -f 0 27 -b 126 141 >> client_log.txt
 sleep 3
-python3 client.py -f 0 41 -b 141 
+python3 client_lathide.py -f 0 41 -b 141 
 sleep 3
-python3 client.py -f 0 41 -b 141  >> client_log.txt
+python3 client_lathide.py -f 0 41 -b 141  >> client_log.txt
 sleep 3
-python3 client.py -f 0 79 -b 141 
+python3 client_lathide.py -f 0 79 -b 141 
 sleep 3
-python3 client.py -f 0 79 -b 141  >> client_log.txt
+python3 client_lathide.py -f 0 79 -b 141  >> client_log.txt
 sleep 3
-python3 client.py -f 0 109 -b 141 
+python3 client_lathide.py -f 0 109 -b 141 
 sleep 3
-python3 client.py -f 0 109 -b 141  >> client_log.txt
+python3 client_lathide.py -f 0 109 -b 141  >> client_log.txt
 sleep 3
-python3 client.py -f 0 53 -b 141 
+python3 client_lathide.py -f 0 53 -b 141 
 sleep 3
-python3 client.py -f 0 53 -b 141  >> client_log.txt
+python3 client_lathide.py -f 0 53 -b 141  >> client_log.txt
 sleep 3
-python3 client.py -f 0 47 -b 141 
+python3 client_lathide.py -f 0 47 -b 141 
 sleep 3
-python3 client.py -f 0 47 -b 141  >> client_log.txt
+python3 client_lathide.py -f 0 47 -b 141  >> client_log.txt
 sleep 3
-python3 client.py -f 0 108 -b 141 
+python3 client_lathide.py -f 0 108 -b 141 
 sleep 3
-python3 client.py -f 0 108 -b 141  >> client_log.txt
+python3 client_lathide.py -f 0 108 -b 141  >> client_log.txt
 sleep 3
-python3 client.py -f 0 11 -b 133 141
+python3 client_lathide.py -f 0 11 -b 133 141
 sleep 3
-python3 client.py -f 0 11 -b 133 141 >> client_log.txt
+python3 client_lathide.py -f 0 11 -b 133 141 >> client_log.txt
 sleep 3
-python3 client.py -f 0 5 -b 133 141
+python3 client_lathide.py -f 0 5 -b 133 141
 sleep 3
-python3 client.py -f 0 5 -b 133 141 >> client_log.txt
+python3 client_lathide.py -f 0 5 -b 133 141 >> client_log.txt
 sleep 3
-python3 client.py -f 0 55 -b 141 
+python3 client_lathide.py -f 0 55 -b 141 
 sleep 3
-python3 client.py -f 0 55 -b 141  >> client_log.txt
+python3 client_lathide.py -f 0 55 -b 141  >> client_log.txt
 sleep 3
-python3 client.py -f 0  -b 115 141
+python3 client_lathide.py -f 0  -b 115 141
 sleep 3
-python3 client.py -f 0  -b 115 141 >> client_log.txt
+python3 client_lathide.py -f 0  -b 115 141 >> client_log.txt
 sleep 3
-python3 client.py -f 0 91 -b 141 
+python3 client_lathide.py -f 0 91 -b 141 
 sleep 3
-python3 client.py -f 0 91 -b 141  >> client_log.txt
+python3 client_lathide.py -f 0 91 -b 141  >> client_log.txt
 sleep 3
-python3 client.py -f 0 39 -b 126 141
+python3 client_lathide.py -f 0 39 -b 126 141
 sleep 3
-python3 client.py -f 0 39 -b 126 141 >> client_log.txt
+python3 client_lathide.py -f 0 39 -b 126 141 >> client_log.txt
 sleep 3
-python3 client.py -f 0 90 -b 141 
+python3 client_lathide.py -f 0 90 -b 141 
 sleep 3
-python3 client.py -f 0 90 -b 141  >> client_log.txt
+python3 client_lathide.py -f 0 90 -b 141  >> client_log.txt
 sleep 3
-python3 client.py -f 0 33 -b 126 141
+python3 client_lathide.py -f 0 33 -b 126 141
 sleep 3
-python3 client.py -f 0 33 -b 126 141 >> client_log.txt
+python3 client_lathide.py -f 0 33 -b 126 141 >> client_log.txt
 sleep 3
-python3 client.py -f 0 61 -b 141 
+python3 client_lathide.py -f 0 61 -b 141 
 sleep 3
-python3 client.py -f 0 61 -b 141  >> client_log.txt
+python3 client_lathide.py -f 0 61 -b 141  >> client_log.txt
 sleep 3
-python3 client.py -f 0 73 -b 141 
+python3 client_lathide.py -f 0 73 -b 141 
 sleep 3
-python3 client.py -f 0 73 -b 141  >> client_log.txt
+python3 client_lathide.py -f 0 73 -b 141  >> client_log.txt
 sleep 3
-python3 client.py -f 0 72 -b 141 
+python3 client_lathide.py -f 0 72 -b 141 
 sleep 3
-python3 client.py -f 0 72 -b 141  >> client_log.txt
+python3 client_lathide.py -f 0 72 -b 141  >> client_log.txt
 sleep 3
-python3 client.py -f 0 41 -b 126 141
+python3 client_lathide.py -f 0 41 -b 126 141
 sleep 3
-python3 client.py -f 0 41 -b 126 141 >> client_log.txt
+python3 client_lathide.py -f 0 41 -b 126 141 >> client_log.txt
 sleep 3
-python3 client.py -f 0 79 -b 126 141
+python3 client_lathide.py -f 0 79 -b 126 141
 sleep 3
-python3 client.py -f 0 79 -b 126 141 >> client_log.txt
+python3 client_lathide.py -f 0 79 -b 126 141 >> client_log.txt
 sleep 3
-python3 client.py -f 0 53 -b 126 141
+python3 client_lathide.py -f 0 53 -b 126 141
 sleep 3
-python3 client.py -f 0 53 -b 126 141 >> client_log.txt
+python3 client_lathide.py -f 0 53 -b 126 141 >> client_log.txt
 sleep 3
-python3 client.py -f 0 47 -b 126 141
+python3 client_lathide.py -f 0 47 -b 126 141
 sleep 3
-python3 client.py -f 0 47 -b 126 141 >> client_log.txt
+python3 client_lathide.py -f 0 47 -b 126 141 >> client_log.txt
 sleep 3
-python3 client.py -f 0 55 -b 126 141
+python3 client_lathide.py -f 0 55 -b 126 141
 sleep 3
-python3 client.py -f 0 55 -b 126 141 >> client_log.txt
+python3 client_lathide.py -f 0 55 -b 126 141 >> client_log.txt
 sleep 3
 python3 local.py -p 0 141
 sleep 3
 python3 local.py -p 0 141 >> client_log.txt
 sleep 3
-python3 client.py -f 0 61 -b 126 141
+python3 client_lathide.py -f 0 61 -b 126 141
 sleep 3
-python3 client.py -f 0 61 -b 126 141 >> client_log.txt
+python3 client_lathide.py -f 0 61 -b 126 141 >> client_log.txt
 sleep 3
-python3 client.py -f 0 13 -b 115 141
+python3 client_lathide.py -f 0 13 -b 115 141
 sleep 3
-python3 client.py -f 0 13 -b 115 141 >> client_log.txt
+python3 client_lathide.py -f 0 13 -b 115 141 >> client_log.txt
 sleep 3
-python3 client.py -f 0  -b 97 141
+python3 client_lathide.py -f 0  -b 97 141
 sleep 3
-python3 client.py -f 0  -b 97 141 >> client_log.txt
+python3 client_lathide.py -f 0  -b 97 141 >> client_log.txt
 sleep 3
-python3 client.py -f 0 73 -b 126 141
+python3 client_lathide.py -f 0 73 -b 126 141
 sleep 3
-python3 client.py -f 0 73 -b 126 141 >> client_log.txt
+python3 client_lathide.py -f 0 73 -b 126 141 >> client_log.txt
 sleep 3
-python3 client.py -f 0 13 -b 133 141
+python3 client_lathide.py -f 0 13 -b 133 141
 sleep 3
-python3 client.py -f 0 13 -b 133 141 >> client_log.txt
+python3 client_lathide.py -f 0 13 -b 133 141 >> client_log.txt
 sleep 3
-python3 client.py -f 0 72 -b 126 141
+python3 client_lathide.py -f 0 72 -b 126 141
 sleep 3
-python3 client.py -f 0 72 -b 126 141 >> client_log.txt
+python3 client_lathide.py -f 0 72 -b 126 141 >> client_log.txt
 sleep 3
-python3 client.py -f 0 11 -b 126 141
+python3 client_lathide.py -f 0 11 -b 126 141
 sleep 3
-python3 client.py -f 0 11 -b 126 141 >> client_log.txt
+python3 client_lathide.py -f 0 11 -b 126 141 >> client_log.txt
 sleep 3
-python3 client.py -f 0 91 -b 126 141
+python3 client_lathide.py -f 0 91 -b 126 141
 sleep 3
-python3 client.py -f 0 91 -b 126 141 >> client_log.txt
+python3 client_lathide.py -f 0 91 -b 126 141 >> client_log.txt
 sleep 3
-python3 client.py -f 0 90 -b 126 141
+python3 client_lathide.py -f 0 90 -b 126 141
 sleep 3
-python3 client.py -f 0 90 -b 126 141 >> client_log.txt
+python3 client_lathide.py -f 0 90 -b 126 141 >> client_log.txt
 sleep 3
-python3 client.py -f 0 97 -b 126 141
+python3 client_lathide.py -f 0 97 -b 126 141
 sleep 3
-python3 client.py -f 0 97 -b 126 141 >> client_log.txt
+python3 client_lathide.py -f 0 97 -b 126 141 >> client_log.txt
 sleep 3
-python3 client.py -f 0 126 -b 141 
+python3 client_lathide.py -f 0 126 -b 141 
 sleep 3
-python3 client.py -f 0 126 -b 141  >> client_log.txt
+python3 client_lathide.py -f 0 126 -b 141  >> client_log.txt
 sleep 3
-python3 client.py -f 0 127 -b 141 
+python3 client_lathide.py -f 0 127 -b 141 
 sleep 3
-python3 client.py -f 0 127 -b 141  >> client_log.txt
+python3 client_lathide.py -f 0 127 -b 141  >> client_log.txt
 sleep 3
-python3 client.py -f 0 133 -b 141 
+python3 client_lathide.py -f 0 133 -b 141 
 sleep 3
-python3 client.py -f 0 133 -b 141  >> client_log.txt
+python3 client_lathide.py -f 0 133 -b 141  >> client_log.txt
 sleep 3
-python3 client.py -f 0  -b 79 141
+python3 client_lathide.py -f 0  -b 79 141
 sleep 3
-python3 client.py -f 0  -b 79 141 >> client_log.txt
+python3 client_lathide.py -f 0  -b 79 141 >> client_log.txt
 sleep 3
-python3 client.py -f 0 25 -b 115 141
+python3 client_lathide.py -f 0 25 -b 115 141
 sleep 3
-python3 client.py -f 0 25 -b 115 141 >> client_log.txt
+python3 client_lathide.py -f 0 25 -b 115 141 >> client_log.txt
 sleep 3
 mv client_log.txt real_test_1965.txt
